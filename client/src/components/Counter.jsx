21b5f04a1247793cfa84d92c56bb8071217b1798
Juan C Muñoz <juancarlosmpdev@gmail.com>
2024-05-15 20:24:41 -0500

    Second-Commit: arreglado el problema de la distribucion del componente 1.

--- client/src/components/Counter.jsx
+++ client/src/components/Counter.jsx
@@ -17,7 +17,7 @@ const Counter = () => {
     }
 
   return (
-    <div className='w-[1024px] h-full flex justify-center items-center'>
+    <div className='w-full h-full flex justify-center items-center'>
         <div className='flex flex-col items-center justify-center my-16 bg-[#F4D06F] w-80 h-48 rounded-lg shadow-md'>
             <h1 className='font-extrabold text-[25px]'>Counter with React</h1>
             <h2 className='text-[40px] font-semibold my-5'>{count}</h2>
